17770a6d2649c441ab1bbd63af71a4a0052761dc
Justinas Stankevicius <justinas@justinas.org>
2022-02-27 21:51:28 +0200

    Handle offline streams / bad URLs

--- streamdemand/__init__.py
+++ streamdemand/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, abort, redirect, url_for
 import streamlink
 
 app = Flask(__name__)
@@ -9,4 +9,7 @@ def stream(stream_url):
         stream_url = f"https://{stream_url}"
 
     streams = streamlink.streams(stream_url)
+    if not streams:
+        abort(404, f"No streams found for {stream_url}")
+
     return redirect(streams['best'].url)
