b306d011d1e312a8b35794a79b553ab082793442
Justinas Stankevicius <justinas@justinas.org>
2022-02-28 16:07:41 +0200

    Handle NoPluginError

--- streamdemand/__init__.py
+++ streamdemand/__init__.py
@@ -12,7 +12,11 @@ def stream(stream_url):
     if request.query_string:
         stream_url += f"?{request.query_string.decode('utf-8')}"
 
-    streams = streamlink.streams(stream_url)
+    try:
+        streams = streamlink.streams(stream_url)
+    except streamlink.exceptions.NoPluginError:
+        abort(400, f"No plugin exists for {stream_url}")
+
     if not streams:
         abort(404, f"No streams found for {stream_url}")
 
