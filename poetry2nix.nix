d25b72a4e85cc9e1115a5f3fb1f5174e9766e840
Justinas Stankevicius <justinas@justinas.org>
2024-06-01 17:48:27 +0300

    Update poetry2nix

--- poetry2nix.nix
+++ poetry2nix.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url = "https://github.com/nix-community/poetry2nix/archive/7acb78166a659d6afe9b043bb6fe5cb5e86bb75e.tar.gz";
-  sha256 = "sha256-QSGP2J73HQ4gF5yh+MnClv2KUKzcpTmikdmV8ULfq2E=";
+  url = "https://github.com/nix-community/poetry2nix/archive/11e97e742da5b4e43c27cfe13fca904e82fd4e56.tar.gz";
+  sha256 = "sha256-MTivB5fsfaPtWwcibBhs20hHYEUe9o9bTRXsGJjQRso=";
 }
